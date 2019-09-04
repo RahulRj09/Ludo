7feadd8fd93795c8d2be8fe8aa99b3295cd35499
RahulRj09 <rahul18@navgurukul.org>
2019-09-04 12:01:11 +0530

    Add. getId method for get coin id

--- src/main/java/ludo/Coin.java
+++ src/main/java/ludo/Coin.java
@@ -1,4 +1,12 @@
 package ludo;
 
 public class Coin {
+    int id;
+    public Coin(int id) {
+        this.id = id;
+    }
+
+    public Coin getId() {
+        return this.id;
+    }
 }
--- src/main/java/ludo/Player.java
+++ src/main/java/ludo/Player.java
@@ -1,8 +1,16 @@
 package ludo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+    List<Coin> coins = new ArrayList<>();
     Color color;
     public Player(Color color) {
         this.color = color;
+        for (int i = 0; i <=3 ; i++) {
+            Coin coin = new Coin(i);
+            this.coins.add(coin.getId());
+        }
     }
 }
