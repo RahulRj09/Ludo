01d0f8f8c89abe357c5a18a2fee6d6f6a6a3475f
RahulRj09 <rahul18@navgurukul.org>
2019-09-04 11:45:44 +0530

    Add. constructor in color class for startingPosition

--- src/main/java/ludo/Color.java
+++ src/main/java/ludo/Color.java
@@ -1,4 +1,13 @@
 package ludo;
 
 public enum Color {
+    RED(0), GREEN(13), YELLOW(26),BLUE(39);
+    private int startingPosition;
+    Color(int startingPosition) {
+        this.startingPosition = startingPosition;
+    }
+
+    public int getStartingPosition() {
+        return startingPosition;
+    }
 }
--- src/main/java/ludo/LudoRunner.java
+++ src/main/java/ludo/LudoRunner.java
@@ -1,6 +1,5 @@
 package ludo;
 
-import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
 
