f8f069f1519499e4b136f69f88692df0136606ec
RahulRj09 <rahul18@navgurukul.org>
2019-09-05 09:35:15 +0530

    Add. getEndingPosition method in Color class for get endingPosition

--- src/main/java/ludo/Color.java
+++ src/main/java/ludo/Color.java
@@ -1,11 +1,17 @@
 package ludo;
 
 public enum Color {
-    RED(0), GREEN(13), YELLOW(26), BLUE(39);
+    RED(0, 57), GREEN(13, 70), YELLOW(26, 83), BLUE(39, 96);
     private int startingPosition;
+    private int endingPosition;
 
-    Color(int startingPosition) {
+    Color(int startingPosition, int endingPosition) {
         this.startingPosition = startingPosition;
+        this.endingPosition = endingPosition;
+    }
+
+    public int getEndingPosition() {
+        return endingPosition;
     }
 
     public int getStartingPosition() {
