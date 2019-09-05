513600c5a921ed61bd5e3a815044c71447ce52c1
RahulRj09 <rahul18@navgurukul.org>
2019-09-05 22:02:11 +0530

    Add. Green class for Green coin

---
+++ src/main/java/ludo/Green.java
@@ -0,0 +1,12 @@
+package ludo;
+
+import java.util.Arrays;
+
+public class Green extends Home {
+    public Green() {
+        this.color = "Green";
+        this.startingPosition = 26;
+        this.endingPosition = 83;
+        this.coins = Arrays.asList(new Coin(), new Coin(), new Coin(), new Coin());
+    }
+}
